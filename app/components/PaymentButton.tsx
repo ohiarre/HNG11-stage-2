e54221a89729aec241c53cd9d9c992a430efccc6
Abdulmuizz Abdulmalik <malikahtaan@gmail.com>
2024-07-09 07:28:55 +0100

    checkout btn

--- app/components/PaymentButton.tsx
+++ app/components/PaymentButton.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function PaymentButton({}: Props) {
   return (
     <Link href="/checkout" passHref>
-      <button className='bg-black text-white px-2 py-2 w-40 text-center text-sm rounded-md'>
+      <button className='bg-black text-white px-2 py-2 my-5 w-40 text-center text-sm rounded-md'>
         <p>Proceed to Payment</p>
       </button>
     </Link>

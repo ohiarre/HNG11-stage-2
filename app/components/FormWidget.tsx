e71faef58121947328d267f4d5f5352a76ebfdee
Abdulmuizz Abdulmalik <malikahtaan@gmail.com>
2024-07-09 07:36:20 +0100

    form visual fix

--- app/components/FormWidget.tsx
+++ app/components/FormWidget.tsx
@@ -8,7 +8,7 @@ function FormWidget({}: Props) {
         <form className='flex flex-col justify-center md:justify-start space-x-3 space-y-5 p-3 py-5'>
             <section className='bg-[#FAFBF6] p-5 rounded-md mb-7'>
             <label htmlFor="country">
-            <h2>DELIVERY COUNTRY:</h2>
+            <h2 className='pb-5'>DELIVERY COUNTRY:</h2>
             <select className="p-2 focus:outline-none bg-white" id="country" name="country">
                 <option value="ng">Nigeria</option>
                 <option value="ca">Canada</option>
@@ -34,20 +34,20 @@ function FormWidget({}: Props) {
             </section>
 
             <section className='bg-[#FAFBF6] p-5 rounded-md'>
-  <h2>DELIVERY DETAILS</h2>
+  <h2 className='pb-5'>DELIVERY DETAILS:</h2>
   
   <label htmlFor="firstName" className='block py-2'>
-    <h3>FIRST NAME</h3>
+    <h4>FIRST NAME</h4>
     <input className="text-black font-extralight text-xs focus:outline-none p-2 w-[300px] rounded-md" type="text" id="firstName" name="firstName" placeholder='Enter your first name' required />
   </label>
 
   <label htmlFor="lastName" className='block py-2'>
-    <h3>LAST NAME</h3>
+    <h4>LAST NAME</h4>
     <input className="text-black font-extralight text-xs focus:outline-none p-2 w-[300px] rounded-md" type="text" id="lastName" name="lastName" placeholder='Enter your last name' required />
   </label>
 
   <label htmlFor="address" className='block py-2'>
-    <h3>ADDRESS</h3>
+    <h4>ADDRESS</h4>
     <input className="text-black font-extralight text-xs focus:outline-none p-2 w-[300px] rounded-md" type="text" id="address" name="address" placeholder='Enter your address' required />
   </label>
 </section>
